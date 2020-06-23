9f39de321d01231c3085650d74448c2e352e0337
ChrisChroma <chrischroma1030@gmail.com>
2020-06-22 22:52:36 -0500

    card link and details page

--- main_app/views.py
+++ main_app/views.py
@@ -1,20 +1,8 @@
 from django.shortcuts import render
+from .models import Games
 
 # Add the following import
 from django.http import HttpResponse
-
-class Game:  # Note that parens are optional if not inheriting from another class
-  def __init__(self, name, console, description, release_date):
-    self.name = name
-    self.console = console
-    self.description = description
-    self.release_date = release_date
-
-games = [
-  Game('Legend of Zelda', 'NES', 'action adventure game', 1986),
-  Game('Marios Bros', 'NES', 'ground breaking platformer', 1983),
-  Game('World of Warcraft', 'PC', 'largest current western MMORPG', 2004)
-]
 
 # Define the home view
 def home(request):
@@ -24,4 +12,9 @@ def about(request):
     return render(request, 'about.html')
 
 def games_index(request):
+    games = Games.objects.all()
     return render(request, 'games/index.html', { 'games': games })
+
+def games_detail(request, games_id):
+    game = Games.objects.get(id=games_id)
+    return render(request, 'games/detail.html', { 'game': game })
