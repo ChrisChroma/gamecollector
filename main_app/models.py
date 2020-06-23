38585d77adb5ed2dea73abc8052022bddead57ff
ChrisChroma <chrischroma1030@gmail.com>
2020-06-22 23:21:28 -0500

    CRUD

--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 class Games(models.Model):
@@ -9,3 +10,6 @@ class Games(models.Model):
 
   def __str__(self):
     return self.name
+
+  def get_absolute_url(self):
+    return reverse('detail', kwargs={'games_id': self.id})
--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,23 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Games
 
 # Add the following import
 from django.http import HttpResponse
+
+class GameCreate(CreateView):
+    model = Games
+    fields = '__all__'
+    success_url = '/games/'
+
+class GameUpdate(UpdateView):
+    model = Games
+    fields = ['console', 'description', 'release_date']
+
+class GameDelete(DeleteView):
+    model = Games
+    success_url = '/games/'
+
 
 # Define the home view
 def home(request):
