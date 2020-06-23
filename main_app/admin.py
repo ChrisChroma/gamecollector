23b0e6ea6f1a7287313b2e8432e039d6c42061cc
ChrisChroma <chrischroma1030@gmail.com>
2020-06-22 23:55:24 -0500

    ERD Stuff

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Games
+from .models import Games, Reviews
 # Register your models here.
 admin.site.register(Games)
+admin.site.register(Reviews)
--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 from django.urls import reverse
 
+RATING = (
+  ('E', 'Everyone'),
+  ('T', 'Teen'),
+  ('M', 'Mature')
+)
+
 # Create your models here.
 class Games(models.Model):
   name = models.CharField(max_length=100)
@@ -13,3 +19,16 @@ class Games(models.Model):
 
   def get_absolute_url(self):
     return reverse('detail', kwargs={'games_id': self.id})
+
+class Reviews(models.Model):
+  rating = models.CharField(
+    max_length=1,
+    choices=RATING,
+    default=RATING[0][0]
+  )
+
+  game = models.ForeignKey(Games, on_delete=models.CASCADE)
+
+  def __str__(self):
+    # Nice method for obtaining the friendly value of a Field.choice
+    return f"Game is rated {self.rating}"
